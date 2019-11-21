f0af73d025d33de6b7f6e65c7768a14b1c77ea72
manucirne <manucirne@gmail.com>
2019-11-21 12:08:15 -0300

    com as instalações necessárias

---
+++ init.sh
@@ -0,0 +1,7 @@
+sudo apt-get update -y
+sudo apt-get install python3 -y
+sudo apt-get install python3-pip -y
+pip3 install flask
+pip3 install flask_restful
+pip3 install requests
+python3 api_redirect.py
